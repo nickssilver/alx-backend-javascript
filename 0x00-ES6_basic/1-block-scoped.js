b3d0971f0349de68759c4eebbfc760303e684601
nickssilver <51230602+nickssilver@users.noreply.github.com>
2023-04-17 23:37:23 +0100

    👽️

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
-
-  if (trueOrFalse) {
-    task = true;
-    task2 = false;
+    const task = false;
+    const task2 = true;
+  
+    if (trueOrFalse) {
+      const task = true; // eslint-disable-line no-unused-vars
+      const task2 = false; // eslint-disable-line no-unused-vars
+    }
+  
+    return [task, task2];
   }
-
-  return [task, task2];
-}
