db96a22a92b8360a4b3be823344b5b8d490994c8
nickssilver <51230602+nickssilver@users.noreply.github.com>
2023-04-18 01:14:01 +0100

    :hammer:

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (const value of array) {
-    const index = array.indexOf(value);
-    array[index] = appendString + value;
+  const newArray = [];
+  for (const idx of array) {
+    newArray.push(appendString + idx);
   }
 
-  return array;
+  return newArray;
 }
