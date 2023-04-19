fe499a35c232b5b33b3e2d97815f675509e0267c
nickssilver <51230602+nickssilver@users.noreply.github.com>
2023-04-19 22:18:00 +0100

    promiseIMp

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,10 @@
+function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // Your API call or other asynchronous operation
+    // On success, call resolve with the result
+    // On failure, call reject with the error
+
+    // Example: Resolve immediately with a dummy value
+    resolve('Dummy value');
+  });
+}
