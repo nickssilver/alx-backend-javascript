694b50ff98ae8be801bb44366395099da870313d
nickssilver <51230602+nickssilver@users.noreply.github.com>
2023-04-21 01:38:08 +0300

    :wrench:

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,14 +1,22 @@
-export default class Building {
-    constructor(sqft = 0) {
-      if (this.constructor !== Building
-          && typeof this.evacuationWarningMessage !== 'function') {
-        throw new Error('Class extending Building must override evacuationWarningMessage');
-      }
-  
-      this._sqft = sqft;
+/* eslint-disable no-underscore-dangle */
+class Building {
+  constructor(sqft) {
+    if (
+      this.constructor !== Building
+      && typeof this.evacuationWarningMessage !== 'function'
+    ) {
+      throw Error(
+        'Class extending Building must override evacuationWarningMessage',
+      );
     }
-  
-    get sqft() {
-      return this._sqft;
-    }
+
+    this._sqft = sqft;
   }
+
+  // sqft
+  get sqft() {
+    return this._sqft;
+  }
+}
+
+export default Building;
