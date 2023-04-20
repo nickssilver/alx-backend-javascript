c454366349a84ce582a60f782f3fdf49abd36e96
nickssilver <51230602+nickssilver@users.noreply.github.com>
2023-04-21 01:36:02 +0300

    fix_:wrench:

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,19 +1,19 @@
-export default class HolbertonClass {
-    constructor(size, location) {
-      if (typeof size !== 'number') {
-        throw new TypeError('Size must be a number');
-      } else if (typeof location !== 'string') {
-        throw new TypeError('Location must be a string');
-      }
-  
-      this._size = size;
-      this._location = location;
+/* eslint-disable no-underscore-dangle */
+class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') {
+      return this._size;
     }
-  
-    [Symbol.toPrimitive](hint) {
-      if (hint === 'number') {
-        return this._size;
-      }
+    if (hint === 'string') {
       return this._location;
     }
+    return this;
   }
+}
+
+export default HolbertonClass;
