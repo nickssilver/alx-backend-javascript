5e7e35df83e2364bbbdf7db1142cb418f6aae579
nickssilver <51230602+nickssilver@users.noreply.github.com>
2023-04-21 01:37:18 +0300

    Fix_:wrench:

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,16 +1,13 @@
-export default class Airport {
-    constructor(name, code) {
-      if (typeof name !== 'string') {
-        throw new TypeError('Name must be a string');
-      } else if (typeof code !== 'string') {
-        throw new TypeError('Code must be a string');
-      }
-  
-      this._name = name;
-      this._code = code;
-    }
-  
-    get [Symbol.toStringTag]() {
-      return `${this._code}`;
-    }
+/* eslint-disable no-underscore-dangle */
+class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
   }
+
+  get [Symbol.toStringTag]() {
+    return this._code;
+  }
+}
+
+export default Airport;
